f5eaabdaed82c9d6df5d91073bfe6b6effa2cd03
pranto <pranto.tap@gmail.com>
2023-11-21 19:50:24 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function myFunction() {
+    var x = document.getElementById("DropDownMenu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function myFunctiontop() {
+    var x = document.getElementById("DropDownMenutwo");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
